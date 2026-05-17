0a6740ef57c1a45d10b500322a3ea1a4772afbf2
hphien444 <153250177+hphien444@users.noreply.github.com>
2026-05-17 15:31:37 +0700

    Sync launcher update after ROS launcher PID fix 2026-05-17 15:30

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
 header('x-amz-meta-bootstrap-version: 1000000');
-header('x-amz-meta-bootstrap-size: 5078876');
-header('x-amz-meta-bootstrap-object: 38e55027b4fcd4404e6837b22433f48fb62ff1a0e07b5bc6e20b7d3005aeafa3');
-header('x-amz-meta-branch-version: 1779004759');
-header('x-amz-meta-branch-manifest: e2833c7890910b4a85bdbbc091988f99f8e12be9cabf425542df08e02d838e01');
+header('x-amz-meta-bootstrap-size: 5077960');
+header('x-amz-meta-bootstrap-object: c9f44fc309785581a32c3255de4afc89e73841cd4d4ad74ea7a2091ca0c64789');
+header('x-amz-meta-branch-version: 1779006688');
+header('x-amz-meta-branch-manifest: f6a1ae47305c5583ff14262de711771b3a227838e04f26f2e49e2017febccf64');
 echo "OK";
 ?>
