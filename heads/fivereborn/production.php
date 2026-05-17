f6bf7b3923a3b3c2ca47e9d7a5a5fe041012a996
hphien444 <hphien444@users.noreply.github.com>
2026-05-17 22:45:58 +0700

    Publish bootstrap 1000004

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000003');
-header('x-amz-meta-bootstrap-size: 5700820');
-header('x-amz-meta-bootstrap-object: 66af6283e298f0233f409d2b37456cb7340d28fef9d6d3c7cb901a3715dcec0d');
-header('x-amz-meta-branch-version: 1779032295');
+header('x-amz-meta-bootstrap-version: 1000004');
+header('x-amz-meta-bootstrap-size: 5700252');
+header('x-amz-meta-bootstrap-object: 5ac85d2625788a1fa5822c87e05cca81843ec3bfc4aa531a9a80a6a4058f05ec');
+header('x-amz-meta-branch-version: 1779032726');
 header('x-amz-meta-branch-manifest: 705039505abd3790310c56cc85bc7bb88c6ee51fbde21644ef43ce35df2fa122');
 echo "OK";
 ?>
