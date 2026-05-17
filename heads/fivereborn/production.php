cfca558d4bb173cc5895c238df53d94de029d668
hphien444 <153250177+hphien444@users.noreply.github.com>
2026-05-17 15:50:44 +0700

    Sync launcher update after extra-natives-five crash fix 2026-05-17 15:48

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000000');
 header('x-amz-meta-bootstrap-size: 5077960');
 header('x-amz-meta-bootstrap-object: c9f44fc309785581a32c3255de4afc89e73841cd4d4ad74ea7a2091ca0c64789');
-header('x-amz-meta-branch-version: 1779006688');
-header('x-amz-meta-branch-manifest: f6a1ae47305c5583ff14262de711771b3a227838e04f26f2e49e2017febccf64');
+header('x-amz-meta-branch-version: 1779007843');
+header('x-amz-meta-branch-manifest: 2e0807d611800fd2fb0e8d83f1902e6428863fb5cabdd1e62b7700af225dcacc');
 echo "OK";
 ?>
