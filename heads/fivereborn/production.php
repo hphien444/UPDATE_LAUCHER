1dd8481f3b46ceae006c72e9ebb1b30b0271e703
hphien444 <hphien444@users.noreply.github.com>
2026-05-17 22:38:16 +0700

    Repair missing TLS DLL in standalone bootstrap

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000002');
-header('x-amz-meta-bootstrap-size: 5699020');
-header('x-amz-meta-bootstrap-object: 19ca62b4bf92ded8c3c9bdf16c36eca6c68e2ab57d5036ba2339d277a8bf9c64');
-header('x-amz-meta-branch-version: 1779031978');
+header('x-amz-meta-bootstrap-version: 1000003');
+header('x-amz-meta-bootstrap-size: 5700820');
+header('x-amz-meta-bootstrap-object: 66af6283e298f0233f409d2b37456cb7340d28fef9d6d3c7cb901a3715dcec0d');
+header('x-amz-meta-branch-version: 1779032295');
 header('x-amz-meta-branch-manifest: 705039505abd3790310c56cc85bc7bb88c6ee51fbde21644ef43ce35df2fa122');
 echo "OK";
 ?>
