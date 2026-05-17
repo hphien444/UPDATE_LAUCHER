6efa3a64f519d7199fa7f56e15dbe10461a69a72
hphien444 <hphien444@users.noreply.github.com>
2026-05-17 22:33:06 +0700

    Bump bootstrap for standalone TLS recovery

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000001');
-header('x-amz-meta-bootstrap-size: 5698484');
-header('x-amz-meta-bootstrap-object: 6428f29ff312a8ba31b03a9e64b5c889a6322b71b41b39a4589695a05b719b5e');
-header('x-amz-meta-branch-version: 1779031521');
+header('x-amz-meta-bootstrap-version: 1000002');
+header('x-amz-meta-bootstrap-size: 5699020');
+header('x-amz-meta-bootstrap-object: 19ca62b4bf92ded8c3c9bdf16c36eca6c68e2ab57d5036ba2339d277a8bf9c64');
+header('x-amz-meta-branch-version: 1779031978');
 header('x-amz-meta-branch-manifest: 705039505abd3790310c56cc85bc7bb88c6ee51fbde21644ef43ce35df2fa122');
 echo "OK";
 ?>
